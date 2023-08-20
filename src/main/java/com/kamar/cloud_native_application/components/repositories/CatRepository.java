499fafe832fc3bc0a30aca9ffb274fd745a065b8
kamarbaraka <kamar254baraka@gmail.com>
2023-08-21 00:39:39 +0300

    added a rest api to manage the data
    Co-authored-by: @samkombe
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/com/kamar/cloud_native_application/components/repositories/CatRepository.java
+++ src/main/java/com/kamar/cloud_native_application/components/repositories/CatRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 /**
- * a repository to manage and perform CRUD operations on cat objects.
+ * a repository to manage and perform CRUD operations on cats.
  * @author kamarbaraka.*/
 
 @RepositoryRestResource
